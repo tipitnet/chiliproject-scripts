3b7446a28667d0a990b2b53e2d6babd539e96144
NicoPaez <nicopaez@gmail.com>
2014-10-31 08:42:30 -0300

    Script updated

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
+echo $BUILD_NUMBER >> version.txt
+
+cd /var/www/chiliproject-inviteme
+git pull origin deploy-spike
+bundle install
+touch /var/www/chiliproject-inviteme/tmp/restart.txt
+
+
 # copy .env file
-cp .env $TARGET_LOCATION
-cd $TARGET_LOCATION
+#cp .env $TARGET_LOCATION
+#cd $TARGET_LOCATION
 
 # get latest code
-git pull origin deploy-spike
-source .env
+#git pull origin deploy-spike
+#source .env
 
 # run migrations
-bundle exec rake db:migrate
+#bundle exec rake db:migrate
 
 # restart the app
-touch tmp/restart
+#touch tmp/restart
